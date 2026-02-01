77f14749dd0829d0577d4c70a1e3371aca6adbbd
2201320029 <2201320029ws@cyber-u.ac.jp>
2026-02-01 23:34:21 +0900

    リポジトリ構成をcuに変更

---
+++ myapp/routes/mongo.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'cu2201320029'; 
+
+router.get('/', (req, res) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+        if (err) {
+            console.error(err);
+            res.json({ status: 'error', error: err.message });
+            return;
+        }
+        
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+
+        collection.find({}).toArray((err, docs) => {
+            client.close();
+            if (err) {
+                res.json({ status: 'error', error: err.message });
+            } else {
+                res.json(docs);
+            }
+        });
+    });
+});
+
+module.exports = router;
